7abf1fc80e6dbb7f71e3bd24da1cdcd514b1a51c
Abid Nuri <mdabidnuri@gmail.com>
2021-02-07 13:59:46 +0600

    searching done

--- app.js
+++ app.js
@@ -4,5 +4,25 @@ document.getElementById('search-button').addEventListener('click', function () {
 
 const getTheMealList = () => {
     let searchInput = document.getElementById('search-input').value;
-    console.log(searchInput);
+    // console.log(searchInput);
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            let allMeal = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    allMeal = `                    
+                        <div class="col-md-3">
+                            <div class="meal-container text-center my-3 p-4 bg-dark text-white data-id="${meal.IdMeal}">
+                                <img src="${meal.strMealThumb}" alt="" class="img-fluid rounded">
+                                <p class="mt-3">${meal.strMeal}</p>
+                            </div>
+                        </div>                    
+                    `;
+                });
+            }
+            const mealList = document.getElementById('meal-item');
+            mealList.innerHTML = allMeal;
+        })
 }
